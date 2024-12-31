37d36488e1a4b9aa87566af073a47a6d44b4d6fe
prism <snawalt420@proton.me>
2024-12-31 03:33:30 +0100

    added persist to store profileData

--- src/components/preview/Watermark.tsx
+++ src/components/preview/Watermark.tsx
@@ -5,19 +5,21 @@
  */
 
 export function Watermark() {
-  return (
-    <div style={{
-      position: 'absolute',
-      bottom: '0.25rem',
-      left: '50%',
-      transform: 'translateX(-50%)',
-      textAlign: 'center',
-      color: '#ffffff',
-      fontSize: '0.75rem',
-      padding: '0.5px',
-      opacity: 0.5
-    }}>
-      Made with mbxmunity.netlify.app
-    </div>
-  );
+    return (
+        <div
+            style={{
+                position: "absolute",
+                bottom: "0.25rem",
+                left: "50%",
+                transform: "translateX(-50%)",
+                textAlign: "center",
+                color: "#ffffff",
+                fontSize: "0.75rem",
+                padding: "0.5px",
+                opacity: 0.5,
+            }}
+        >
+            Made with mbxmunity.netlify.app
+        </div>
+    );
 }
