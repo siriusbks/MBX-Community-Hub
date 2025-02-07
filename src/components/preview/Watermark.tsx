37722e251b13276360cbfefee876ac822e396f68
siriusbks <nath.bordat@hotmail.fr>
2025-02-07 13:08:56 +0100

    update branding

--- src/components/preview/Watermark.tsx
+++ src/components/preview/Watermark.tsx
@@ -8,6 +8,6 @@ import { FC } from "react";
 
 export const Watermark: FC = () => (
     <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 text-center text-white text-xs p-[0.5px] opacity-50">
-        Made with mbxmunity.netlify.app
+        Made with mineboxcommunity.com
     </div>
 );
