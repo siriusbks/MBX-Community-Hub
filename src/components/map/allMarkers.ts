0a276e4c84160c31615f732e4e5caaaf386ea09e
siriusbks <nath.bordat@hotmail.fr>
2025-02-17 17:16:16 +0100

    add allMarkers component to consolidate map markers

---
+++ src/components/map/allMarkers.ts
@@ -0,0 +1,25 @@
+/*
+ * MBX, Community Based Project
+ * Copyright (c) 2024 SiriusB_
+ * SPDX-License-Identifier: MIT
+ */
+
+import spawnMarkers from "./spawnMarkers";
+import homeIslandMarkers from "./homeIslandMarkers";
+import kokokoMarkers from "./kokokoMarkers";
+import quadraMarkers from "./quadraMarkers";
+import bambooMarkers from "./bambooMarkers";
+import frostbiteMarkers from "./frostbiteMarkers";
+import sandwhisperMarkers from "./sandwhisperMarkers";
+
+const allMarkers: Record<string, Record<string, any>> = {
+    spawn: spawnMarkers,
+    home_island: homeIslandMarkers,
+    kokoko: kokokoMarkers,
+    quadra_plains: quadraMarkers,
+    bamboo_peak: bambooMarkers,
+    frostbite_fortress: frostbiteMarkers,
+    sandwhisper_dunes: sandwhisperMarkers,
+};
+
+export default allMarkers;
