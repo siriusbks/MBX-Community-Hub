5a52a1dd518b3fc1ea33a35ed9ee5e6753d64fde
Nathan <nath.bordat@hotmail.fr>
2025-04-06 14:07:04 +0200

    remove top border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@
 
 export const Footer = () => {
     return (
-        <footer className="bg-gray-900 border-t border-gray-800 py-6">
+        <footer className="bg-gray-900 border-gray-800 py-6">
             <div className="container mx-auto px-4 text-center">
                 <p className="text-sm text-gray-400 tracking-wide">
                     Made with <span className="text-red-500">❤️</span> for the{" "}
