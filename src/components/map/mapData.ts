91f77bffe3f7c7f223603a21353bce062ab52515
Nathan <nath.bordat@hotmail.fr>
2025-05-08 12:03:43 +0200

    remove the treasure display

--- src/components/map/mapData.ts
+++ src/components/map/mapData.ts
@@ -95,14 +95,7 @@ export const mapData: Record<string, MapDataConfig> = {
         requiredLevel: 20,
         description: "",
         referencePoint: { x: 17, y: 143 },
-        markerRefs: [
-            "treasure_common",
-            "treasure_uncommon",
-            "treasure_rare",
-            "treasure_epic",
-            "treasure_legendary",
-            "treasure_mythic",
-        ],
+        markerRefs: ["treasure_rare", "treasure_epic", "treasure_legendary"],
     },
     end_island: {
         imageUrl: "/assets/media/maps/end_island.png",
