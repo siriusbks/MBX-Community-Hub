14932e34bc8392eb3c55b7de7dc65a4a02528308
Nathan <nath.bordat@hotmail.fr>
2025-05-24 16:40:07 +0200

    fix: adjust opacity and pointer events for Watermark component

--- src/components/preview/Watermark.tsx
+++ src/components/preview/Watermark.tsx
@@ -7,7 +7,7 @@
 import { FC } from "react";
 
 export const Watermark: FC = () => (
-    <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 text-center text-white text-xs p-[0.5px] opacity-50">
+    <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 text-center text-white text-xs opacity-60 select-none pointer-events-none">
         Made with mineboxcommunity.com
     </div>
 );
