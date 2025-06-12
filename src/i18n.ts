afadabf92cfb18691e98a2958004264452ac0eb0
Nathan <nath.bordat@hotmail.fr>
2025-06-12 21:16:24 +0200

    feat: add language detection support using i18next-browser-languagedetector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,27 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n.use(initReactI18next)
-    .use(HttpBackend)
+i18n.use(HttpBackend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
     .init({
-        fallbackLng: "en", // Default language
+        fallbackLng: "en",
         debug: false,
-        interpolation: { escapeValue: false },
         backend: {
             loadPath: "/locales/{{lng}}/{{ns}}.json",
         },
+        detection: {
+            order: ["cookie", "localStorage", "navigator"],
+            caches: ["cookie"],
+            lookupCookie: "mbxcom_lang",
+        },
         ns: ["homepage", "navbar", "profile", "map", "community", "fishing"],
         defaultNS: "homepage",
+        interpolation: {
+            escapeValue: false,
+        },
     });
 
 export default i18n;
