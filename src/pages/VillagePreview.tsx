75c5981525f270d99d090c32d889e69903821d06
Tiny Slime Studio <tinyslimestudio@gmail.com>
2026-06-19 23:34:58 +0200

    feat: Village progress

--- src/pages/VillagePreview.tsx
+++ src/pages/VillagePreview.tsx
@@ -19,6 +19,29 @@ import { Link, useParams } from "react-router-dom"
 export function VillagePreview() {
   const { t, i18n } = useTranslation("maps")
   const params = useParams()
+  const [villageData, setVillageData] = useState<any | null>(null)
+
+  useEffect(() => {
+    fetch("/assets/data/village.json")
+      .then((r) => r.json())
+      .then((data) => {
+        setVillageData(data)
+      })
+      .catch((e) => console.error("Failed to load village data", e))
+  }, [])
+  if (!villageData) {
+    return (
+      <div className="relative page-container flex flex-col items-center pb-24">
+        <PageTitle>{t("village_preview")}</PageTitle>
+        <div>Loading...</div>
+      </div>
+    )
+  }
+
+  const cells = villageData.cells?.[0] ?? {}
+  const cellEntries = Object.entries(cells)
+  const buildings = villageData.buildings?.[0] ?? {}
+  const buildingEntries = Object.entries(buildings)
 
   return (
     <div className="relative page-container flex flex-col items-center pb-24">
@@ -33,99 +56,55 @@ export function VillagePreview() {
               imageRendering: "pixelated",
             }}
           />
-          
-          {/* 4 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-28/138 left-46/140 size-1/14 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 3 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- Miner</p>
-              <p>- Lumberjack</p>
-              <p>- Gathering</p>
-              <p>- Farmer</p>
-            </PopoverContent>
-          </Popover>
-          
-          {/* 3 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-37/138 left-87/140 size-1/14 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 2 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- Fisherman</p>
-              <p>- Pet Trainer</p>
-            </PopoverContent>
-          </Popover>
-          
-          {/* 1 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-51/138 left-66/140 size-1/14 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 1 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- Gathering</p>
-              <p>- Farmer</p>
-              <p>- Pet Trainer</p>
-            </PopoverContent>
-          </Popover>
-          
-          {/* 5 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-83/138 left-30/140 size-1/14 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 4 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- ???</p>
-            </PopoverContent>
-          </Popover>
-          
-          {/* 2 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-90/138 left-57/140 size-1/14 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 1 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- Miner</p>
-              <p>- Lumberjack</p>
-            </PopoverContent>
-          </Popover>
-          
-          {/* 6 */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="absolute top-0 top-103/138 left-102/140 size-1/15 hover:bg-white/10" />
-            </PopoverTrigger>
-            <PopoverContent className="gap-0">
-              <PopoverHeader >
-                <PopoverTitle>Unlocked at Level 5 Village</PopoverTitle>
-                <PopoverDescription>Possible Buildings to Construct:</PopoverDescription>
-              </PopoverHeader>
-              <p>- ???</p>
-            </PopoverContent>
-          </Popover>
+          {cellEntries.map(([cellKey, cellData]) => (
+            <Popover key={cellKey}>
+              <PopoverTrigger asChild>
+                <div
+                  className="absolute size-1/14 hover:bg-white/10"
+                  style={{
+                    left: `${(cellData.x / 140) * 100}%`,
+                    top: `${(cellData.y / 138) * 100}%`,
+                  }}
+                />
+              </PopoverTrigger>
+              <PopoverContent className="gap-0">
+                <PopoverHeader>
+                  <PopoverTitle>
+                    Unlocked at Level {cellData.village_tier_required} Village
+                  </PopoverTitle>
+                  <PopoverDescription>
+                    Possible Buildings to Construct:
+                  </PopoverDescription>
+                </PopoverHeader>
+                <div className="p-2">
+                  {Array.isArray(cellData.allowed_buildings) ? (
+                    cellData.allowed_buildings.map(
+                      (building: string, index: number) => (
+                        <p key={index}>- {building}</p>
+                      )
+                    )
+                  ) : (
+                    <p>- {String(cellData.allowed_buildings)}</p>
+                  )}
+                </div>
+              </PopoverContent>
+            </Popover>
+          ))}
         </span>
         <Card className="w-1/3"></Card>
+      </div>
+      <p>Buildings</p>
+      <div className="grid w-full grid-cols-3 gap-4">
+        {buildingEntries.map(([buildingKey, buildingData]) => (
+          <Card className="w-full p-0 gap-0">
+            {buildingKey}
+            
+            <Card className="w-full p-0">
+              Tier X
+            </Card>
+            
+            </Card>
+        ))}
       </div>
     </div>
   )
