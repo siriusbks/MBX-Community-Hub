77c9f11a26d062c6d61617bd72bc94cb5a826bad
Tiny Slime Studio <tinyslimestudio@gmail.com>
2026-07-23 16:45:12 +0200

    feat: Equipment Slots Scale + Firefox Scrollbar

--- src/components/equipment/EquipmentSlot.tsx
+++ src/components/equipment/EquipmentSlot.tsx
@@ -44,27 +44,27 @@ export const EquipmentSlot: React.FC<Props> = ({
             {equippedItem ? (
               <RarityBorder
                 rarity={rarityId}
-                className="flex h-24 w-24 items-center justify-center transition-transform duration-200 group-hover:scale-110 border-[6px]"
+                className="flex h-24 w-24 items-center justify-center border-[6px] transition-transform duration-200 group-hover:scale-110"
               >
-{equippedItem.category === "CLASS" ? (
-                                    <img
-                                        src={
-                                            equippedItem.image
-                                                ? `${equippedItem.image}`
-                                                : "/media/missingClass.png"
-                                        }
-                                        alt={equippedItem.name}
-                                        className="size-full [image-rendering:pixelated]"
-                                    />
-                                ) : (
-                                    <ItemImage
-                                        itemId={equippedItem.id}
-                                        className="size-full object-contain"
-                                    />
-                                )}
+                {equippedItem.category === "CLASS" ? (
+                  <img
+                    src={
+                      equippedItem.image
+                        ? `${equippedItem.image}`
+                        : "/media/missingClass.png"
+                    }
+                    alt={equippedItem.name}
+                    className="size-full [image-rendering:pixelated]"
+                  />
+                ) : (
+                  <ItemImage
+                    itemId={equippedItem.id}
+                    className="size-full object-contain"
+                  />
+                )}
               </RarityBorder>
             ) : (
-              <div className="flex h-24 w-24 items-center justify-center rounded-lg border-[3px] border-card-dark bg-linear-to-b from-secondary-lighter/50 to-secondary/50 transition-all duration-200 group-hover:scale-110 hover:border-card">
+              <div className="flex size-18 3xl:size-24 items-center justify-center rounded-lg border-[3px] border-card-dark bg-linear-to-b from-secondary-lighter/50 to-secondary/50 transition-all duration-200 group-hover:scale-110 hover:border-card">
                 <Plus className="pointer-events-none h-9 w-9 text-foreground/50" />
               </div>
             )}
@@ -79,59 +79,63 @@ export const EquipmentSlot: React.FC<Props> = ({
               </div>
             )}
 
-            
-            {equippedItem?.level != null && playerLevel < equippedItem.level &&(
-              <div className="absolute inset-x-0 top-1 flex justify-center">
-                <span className="mb-0.5 inline-flex items-center gap-1 rounded-t bg-black/70 px-1.5 py-0.5 text-[10px]  text-red-400">
-                  Level Too Low
-                </span>
-              </div>
-
-            )}
-
-
-           
-
+            {equippedItem?.level != null &&
+              playerLevel < equippedItem.level && (
+                <div className="absolute inset-x-0 top-1 flex justify-center">
+                  <span className="mb-0.5 inline-flex items-center gap-1 rounded-t bg-black/70 px-1.5 py-0.5 text-[10px] text-red-400">
+                    Level Too Low
+                  </span>
+                </div>
+              )}
           </div>
         </TooltipTrigger>
 
         <TooltipContent className="min-w-64 p-0">
           {equippedItem ? (
-            <RarityBorder rarity={rarityId} className="flex w-full text-muted-foreground">
-            <div className="flex flex-col gap-1">
-              <div className="flex items-center  gap-1">
-                <RarityBadge rarity={rarityId} className="text-[10px]" />
-                <span className="truncate text-xs text-white" title={equippedItem.name}>
-                  {equippedItem.name}
-                </span>
-              </div>
+            <RarityBorder
+              rarity={rarityId}
+              className="flex w-full text-muted-foreground"
+            >
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-1">
+                  <RarityBadge rarity={rarityId} className="text-[10px]" />
+                  <span
+                    className="truncate text-xs text-white"
+                    title={equippedItem.name}
+                  >
+                    {equippedItem.name}
+                  </span>
+                </div>
 
-              {equippedItem.stats && (
-                <ul className="mt-1 space-y-0.5">
-                  {Object.entries(equippedItem.stats).map(([stat, range]) => (
-                    <li key={stat}>
-                      <StatItem
-                        stat={stat}
-                        from={range[0]}
-                        to={range[1] ?? range[0]}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
+                {equippedItem.stats && (
+                  <ul className="mt-1 space-y-0.5">
+                    {Object.entries(equippedItem.stats).map(([stat, range]) => (
+                      <li key={stat}>
+                        <StatItem
+                          stat={stat}
+                          from={range[0]}
+                          to={range[1] ?? range[0]}
+                        />
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </RarityBorder>
           ) : (
-            <RarityBorder rarity="vanilla" className="flex w-full text-muted-foreground">
-            <div className="flex flex-col gap-1">
-              <span className="text-xs text-white">{slotLabel}</span>
-              <p className="text-[11px] text-muted-foreground">
-                {t("equip.tooltip.empty")}
-              </p>
-              <p className="text-[11px] text-primary">
-                {t("equip.tooltip.action")}
-              </p>
-            </div>
+            <RarityBorder
+              rarity="vanilla"
+              className="flex w-full text-muted-foreground"
+            >
+              <div className="flex flex-col gap-1">
+                <span className="text-xs text-white">{slotLabel}</span>
+                <p className="text-[11px] text-muted-foreground">
+                  {t("equip.tooltip.empty")}
+                </p>
+                <p className="text-[11px] text-primary">
+                  {t("equip.tooltip.action")}
+                </p>
+              </div>
             </RarityBorder>
           )}
         </TooltipContent>
